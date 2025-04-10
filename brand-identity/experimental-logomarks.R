7636cf9daf5c656005b20bd54c5b8d4dcc679fc9
charliejhadley <charliejhadley@gmail.com>
2025-04-10 13:59:09 +0100

    bulk

---
+++ brand-identity/experimental-logomarks.R
@@ -0,0 +1,117 @@
+logo_equ_solution <- 50 - 75 / {2 * sqrt(2)}
+logo_line_width <- 15
+logo_minor_center_circle_radius <- 37.5
+
+gg_logo_experiment_colourful_ring <- tibble(
+  x = 100 - logo_equ_solution,
+  y = 100 - logo_equ_solution,
+  colour = "secondary"
+) %>%
+  ggplot() +
+  geom_arc(
+    aes(
+      x0 = 0,
+      y0 = 0,
+      r = 3 * logo_minor_center_circle_radius,
+      start = {1/8} * 2 * pi,
+      end = {1/8} * 2 * pi + {2 * pi / 3}
+    ),
+    colour = "black",
+    linewidth = logo_line_width + 5
+  ) +
+  geom_arc(
+    aes(
+      x0 = 0,
+      y0 = 0,
+      r = 3 * logo_minor_center_circle_radius,
+      start = {1/8} * 2 * pi,
+      end = {1/8} * 2 * pi + {2 * pi / 3}
+    ),
+    colour = cols_gpcds$chart_primary,
+    linewidth = logo_line_width
+  ) +
+  geom_arc(
+    aes(
+      x0 = 0,
+      y0 = 0,
+      r = 3 * logo_minor_center_circle_radius,
+      start = {1/8} * 2 * pi + {2 * pi / 3},
+      end =  {1/8} * 2 * pi + {4 * pi / 3}
+    ),
+    colour = "black",
+    linewidth = logo_line_width + 5
+  ) +
+  geom_arc(
+    aes(
+      x0 = 0,
+      y0 = 0,
+      r = 3 * logo_minor_center_circle_radius,
+      start = {1/8} * 2 * pi + {2 * pi / 3},
+      end =  {1/8} * 2 * pi + {4 * pi / 3}
+    ),
+    colour = cols_gpcds$graph_primary,
+    linewidth = logo_line_width
+  ) +
+  geom_arc(
+    aes(
+      x0 = 0,
+      y0 = 0,
+      r = 3 * logo_minor_center_circle_radius,
+      start = {1/8} * 2 * pi + {4 * pi / 3},
+      end =  2 * pi + {1/4} * pi
+    ),
+    colour = "black",
+    linewidth = logo_line_width + 5
+  ) +
+  geom_arc(
+    aes(
+      x0 = 0,
+      y0 = 0,
+      r = 3 * logo_minor_center_circle_radius,
+      start = {1/8} * 2 * pi + {4 * pi / 3},
+      end =  2 * pi + {1/4} * pi
+    ),
+    colour = cols_gpcds$plot_primary,
+    linewidth = logo_line_width
+  ) +
+  geom_point(
+    aes(x, y),
+    fill = cols_gpcds$story_primary,
+    colour = "black",
+    stroke = 4,
+    size = ident_disk_size * ident_minor_disk_factor,
+    pch = 21,
+    show.legend = FALSE
+  ) +
+  # geom_point(
+  #   aes(x, y),
+  #   colour = cols_gpcds$story_tertiary_darker,
+  #   size = ident_disk_size * ident_minor_disk_factor * 0.5,
+  #   show.legend = FALSE
+  # ) +
+  geom_richtext(
+    aes(
+      x = 0,
+      y = 0,
+      label = str_glue("<span style='color:{cols_gpcds$graph_primary};'>G</span>",
+                       "<span style='color:{cols_gpcds$plot_primary};'>P</span>",
+                       "<span style='color:{cols_gpcds$chart_primary};'>C</span>",
+                       "<span style='color:{cols_gpcds$story_primary};'>DS</span>",
+                       .sep = "<span style='color:#000000;'>|</span>")
+    ),
+    family = "Futura",
+    fill = NA,
+    label.color = NA,
+    lineheight = 1,
+    size = ident_textsize * 0.75
+  ) +
+  coord_fixed() +
+  theme_void() +
+  NULL
+
+gg_logo_experiment_colourful_ring %>% 
+  ggsave("brand-identity/logo_experiment_colourful_ring.png",
+         .,
+         width = 8,
+         height = 8,
+         bg = "white")
---
+++ posts/2025-05-01/logo_final.R
@@ -0,0 +1,471 @@
+library("tidyverse")
+library("ggforce")
+library("tidyverse")
+library("GPCDStools")
+library("cjhRutils")
+
+cols_gpcds <- as.list(deframe(select(colours_gpcds, name, hex_code)))
+
+ident_linewidth <- 5
+ident_disk_size <- 25
+ident_textsize <- 40
+ident_minor_disk_factor <- sqrt(5)
+ident_font <- "Futura"
+
+# Solve (x - 50)² + (y - 50)² = 37.5² and y = x
+chart_equ_solution <- 50 - 75 / {2 * sqrt(2)}
+chart_minor_center_circle_radius <- 37.5
+
+logomarkfill_colour_geoms_colourful <- "white"
+logomarkstroke_colour_geoms_colourful <- "#4F4F4F"
+logomarkstroke_size <- 5
+logomarkstroke_segment_adjustment <- 8
+logomarkstroke_curve_adjustment <- 2
+
+logomarkstory_arrow_x_adjustment <- 2.5
+logomarkstory_arrow_y_adjustment <- 15
+logomarkstory_x_coords <- list(min = 25, max = 75)
+
+
+# LOGOMARKS ---------------------------------------------------------------
+
+## ==== Colour mark
+
+gg_logomark_colour <- ggplot() +
+  ## ==== graph
+  annotate("rect",
+           xmin = -100, xmax = 0, ymin = 0, ymax = 100,
+           fill = cols_gpcds$graph_primary,
+           colour = cols_gpcds$graph_primary) +
+  annotate(
+    "segment",
+    x = -20, 
+    xend = -80,
+    y = 50, 
+    yend = 50,
+    linewidth = ident_linewidth + logomarkstroke_segment_adjustment,
+    colour = logomarkstroke_colour_geoms_colourful) +
+  annotate("point",
+           x = c(-20, -80),
+           y = c(50, 50),
+           size = ident_disk_size,
+           colour = logomarkstroke_colour_geoms_colourful,
+           fill = logomarkfill_colour_geoms_colourful,
+           stroke = logomarkstroke_size,
+           pch = 21) +
+  annotate(
+    "segment",
+    x = -20, 
+    xend = -80,
+    y = 50, 
+    yend = 50,
+    linewidth = ident_linewidth + 2,
+    colour = logomarkfill_colour_geoms_colourful) +
+  ## ==== plot
+  annotate("rect",
+           xmin = 0, xmax = 100, ymin = 0, ymax = 100,
+           fill = cols_gpcds$plot_primary,
+           colour = cols_gpcds$plot_primary) +
+  annotate("point",
+           x = c(30, 70),
+           y = c(70, 30),
+           size = ident_disk_size,
+           colour = logomarkstroke_colour_geoms_colourful,
+           fill = logomarkfill_colour_geoms_colourful,
+           stroke = logomarkstroke_size,
+           pch = 21) +
+  ## ==== chart
+  annotate("rect",
+           xmin = -100, xmax = 0, ymin = -100, ymax = 0,
+           fill = cols_gpcds$chart_primary,
+           colour = cols_gpcds$chart_primary) +
+  geom_circle(aes(x0 = -50, y0 = -50, r = chart_minor_center_circle_radius),
+              linewidth = logomarkstroke_size - 1,
+              colour = logomarkstroke_colour_geoms_colourful,
+              fill = logomarkfill_colour_geoms_colourful) +
+  annotate("point",
+           x = -chart_equ_solution,
+           y = -chart_equ_solution,
+           size = ident_disk_size,
+           colour = logomarkstroke_colour_geoms_colourful,
+           fill = logomarkfill_colour_geoms_colourful,
+           stroke = logomarkstroke_size,
+           pch = 21) +
+  ## ==== story
+  annotate("rect",
+           xmin = 0, xmax = 100, ymin = 0, ymax = -100,
+           fill = cols_gpcds$story_primary,
+           colour = cols_gpcds$story_primary) +
+  annotate("point",
+           x = c(25, 75),
+           y = -50,
+           size = ident_disk_size,
+           colour = logomarkstroke_colour_geoms_colourful,
+           fill = logomarkfill_colour_geoms_colourful,
+           stroke = logomarkstroke_size,
+           pch = 21) +
+  annotate("curve",
+           x = logomarkstory_x_coords$min + logomarkstory_arrow_x_adjustment,
+           y = -50 + logomarkstory_arrow_y_adjustment,
+           xend = logomarkstory_x_coords$max - logomarkstory_arrow_x_adjustment,
+           yend = -50 + logomarkstory_arrow_y_adjustment,
+           linewidth = ident_linewidth + logomarkstroke_curve_adjustment,
+           colour = logomarkstroke_colour_geoms_colourful,
+           arrow = arrow(length = unit(0.6, "cm"), type = "closed"),
+           curvature = -0.5,
+           ncp = 1000) +
+  annotate("curve",
+           x = logomarkstory_x_coords$min + logomarkstory_arrow_x_adjustment,
+           y = -50 + logomarkstory_arrow_y_adjustment,
+           xend = logomarkstory_x_coords$max - logomarkstory_arrow_x_adjustment,
+           yend = -50 + logomarkstory_arrow_y_adjustment,
+           linewidth = ident_linewidth - logomarkstroke_curve_adjustment,
+           colour = logomarkfill_colour_geoms_colourful,
+           arrow = arrow(length = unit(0.6, "cm"), type = "closed"),
+           curvature = -0.5,
+           ncp = 1000) +
+  annotate("curve",
+           x = 100 - logomarkstory_x_coords$min - logomarkstory_arrow_x_adjustment,
+           y = -50 - logomarkstory_arrow_y_adjustment,
+           xend = 100 - logomarkstory_x_coords$max + logomarkstory_arrow_x_adjustment,
+           yend = -50 - logomarkstory_arrow_y_adjustment,
+           linewidth = ident_linewidth + logomarkstroke_curve_adjustment,
+           colour = logomarkstroke_colour_geoms_colourful,
+           arrow = arrow(length = unit(0.6, "cm"), type = "closed"),
+           curvature = -0.5,
+           ncp = 1000) +
+  annotate("curve",
+           x = 100 - logomarkstory_x_coords$min - logomarkstory_arrow_x_adjustment,
+           y = -50 - logomarkstory_arrow_y_adjustment,
+           xend = 100 - logomarkstory_x_coords$max + logomarkstory_arrow_x_adjustment,
+           yend = -50 - logomarkstory_arrow_y_adjustment,
+           linewidth = ident_linewidth - logomarkstroke_curve_adjustment,
+           colour = logomarkfill_colour_geoms_colourful,
+           arrow = arrow(length = unit(0.6, "cm"), type = "closed"),
+           curvature = -0.5,
+           ncp = 1000) +
+  ## ==== seperators 
+  geom_segment(aes(y = -100, yend = 100, x = 0, xend = 0),
+               colour = logomarkstroke_colour_geoms_colourful,
+               linewidth = logomarkstroke_size - 2) +
+  geom_segment(aes(x = -100, xend = 100, y = 0, yend = 0),
+               colour = logomarkstroke_colour_geoms_colourful,
+               linewidth = logomarkstroke_size - 2) +
+  annotate("rect",
+           xmin = -100, xmax = 100, ymin = -100, ymax = 100,
+           fill = NA,
+           colour = logomarkstroke_colour_geoms_colourful,
+           linewidth = logomarkstroke_size) +
+  ## ==== scales
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  coord_fixed(xlim = c(-100, 100), ylim = c(-100, 100)) +
+  theme_void()
+
+gg_logomark_colour %>% 
+  ggsave(quarto_here("logomark_colour.png"),
+         .,
+         width = 10,
+         height = 10)
+
+## ==== Black on white
+
+gg_logomark_black_on_white <- ggplot() +
+  ## ==== graph
+  annotate("rect",
+           xmin = -100, xmax = 0, ymin = 0, ymax = 100,
+           fill = "white",
+           colour = cols_gpcds$graph_primary) +
+  annotate(
+    "segment",
+    x = -20, 
+    xend = -80,
+    y = 50, 
+    yend = 50,
+    linewidth = ident_linewidth + logomarkstroke_segment_adjustment,
+    colour = logomarkstroke_colour_geoms_colourful) +
+  annotate("point",
+           x = c(-20, -80),
+           y = c(50, 50),
+           size = ident_disk_size,
+           colour = logomarkstroke_colour_geoms_colourful,
+           fill = logomarkstroke_colour_geoms_colourful,
+           stroke = logomarkstroke_size,
+           pch = 21) +
+  annotate(
+    "segment",
+    x = -20, 
+    xend = -80,
+    y = 50, 
+    yend = 50,
+    linewidth = ident_linewidth + 2,
+    colour = logomarkstroke_colour_geoms_colourful) +
+  ## ==== plot
+  annotate("rect",
+           xmin = 0, xmax = 100, ymin = 0, ymax = 100,
+           fill = "white",
+           colour = cols_gpcds$plot_primary) +
+  annotate("point",
+           x = c(30, 70),
+           y = c(70, 30),
+           size = ident_disk_size,
+           colour = logomarkstroke_colour_geoms_colourful,
+           fill = logomarkstroke_colour_geoms_colourful,
+           stroke = logomarkstroke_size,
+           pch = 21) +
+  ## ==== chart
+  annotate("rect",
+           xmin = -100, xmax = 0, ymin = -100, ymax = 0,
+           fill = "white",
+           colour = cols_gpcds$chart_primary) +
+  geom_circle(aes(x0 = -50, y0 = -50, r = chart_minor_center_circle_radius),
+              linewidth = logomarkstroke_size - 1,
+              colour = logomarkstroke_colour_geoms_colourful,
+              fill = logomarkstroke_colour_geoms_colourful) +
+  annotate("point",
+           x = -chart_equ_solution,
+           y = -chart_equ_solution,
+           size = ident_disk_size,
+           colour = logomarkstroke_colour_geoms_colourful,
+           fill = logomarkfill_colour_geoms_colourful,
+           stroke = logomarkstroke_size,
+           pch = 21) +
+  ## ==== story
+  annotate("rect",
+           xmin = 0, xmax = 100, ymin = 0, ymax = -100,
+           fill = "white",
+           colour = cols_gpcds$story_primary) +
+  annotate("point",
+           x = c(25, 75),
+           y = -50,
+           size = ident_disk_size,
+           colour = logomarkstroke_colour_geoms_colourful,
+           fill = logomarkstroke_colour_geoms_colourful,
+           stroke = logomarkstroke_size,
+           pch = 21) +
+  annotate("curve",
+           x = logomarkstory_x_coords$min + logomarkstory_arrow_x_adjustment,
+           y = -50 + logomarkstory_arrow_y_adjustment,
+           xend = logomarkstory_x_coords$max - logomarkstory_arrow_x_adjustment,
+           yend = -50 + logomarkstory_arrow_y_adjustment,
+           linewidth = ident_linewidth + logomarkstroke_curve_adjustment,
+           colour = logomarkstroke_colour_geoms_colourful,
+           arrow = arrow(length = unit(0.6, "cm"), type = "closed"),
+           curvature = -0.5,
+           ncp = 1000) +
+  annotate("curve",
+           x = logomarkstory_x_coords$min + logomarkstory_arrow_x_adjustment,
+           y = -50 + logomarkstory_arrow_y_adjustment,
+           xend = logomarkstory_x_coords$max - logomarkstory_arrow_x_adjustment,
+           yend = -50 + logomarkstory_arrow_y_adjustment,
+           linewidth = ident_linewidth - logomarkstroke_curve_adjustment,
+           colour = logomarkstroke_colour_geoms_colourful,
+           arrow = arrow(length = unit(0.6, "cm"), type = "closed"),
+           curvature = -0.5,
+           ncp = 1000) +
+  annotate("curve",
+           x = 100 - logomarkstory_x_coords$min - logomarkstory_arrow_x_adjustment,
+           y = -50 - logomarkstory_arrow_y_adjustment,
+           xend = 100 - logomarkstory_x_coords$max + logomarkstory_arrow_x_adjustment,
+           yend = -50 - logomarkstory_arrow_y_adjustment,
+           linewidth = ident_linewidth + logomarkstroke_curve_adjustment,
+           colour = logomarkstroke_colour_geoms_colourful,
+           arrow = arrow(length = unit(0.6, "cm"), type = "closed"),
+           curvature = -0.5,
+           ncp = 1000) +
+  annotate("curve",
+           x = 100 - logomarkstory_x_coords$min - logomarkstory_arrow_x_adjustment,
+           y = -50 - logomarkstory_arrow_y_adjustment,
+           xend = 100 - logomarkstory_x_coords$max + logomarkstory_arrow_x_adjustment,
+           yend = -50 - logomarkstory_arrow_y_adjustment,
+           linewidth = ident_linewidth - logomarkstroke_curve_adjustment,
+           colour = logomarkstroke_colour_geoms_colourful,
+           arrow = arrow(length = unit(0.6, "cm"), type = "closed"),
+           curvature = -0.5,
+           ncp = 1000) +
+  ## ==== seperators 
+  geom_segment(aes(y = -100, yend = 100, x = 0, xend = 0),
+               colour = logomarkstroke_colour_geoms_colourful,
+               linewidth = logomarkstroke_size - 2) +
+  geom_segment(aes(x = -100, xend = 100, y = 0, yend = 0),
+               colour = logomarkstroke_colour_geoms_colourful,
+               linewidth = logomarkstroke_size - 2) +
+  annotate("rect",
+           xmin = -100, xmax = 100, ymin = -100, ymax = 100,
+           fill = NA,
+           colour = logomarkstroke_colour_geoms_colourful,
+           linewidth = logomarkstroke_size) +
+  ## ==== scales
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  coord_fixed(xlim = c(-100, 100), ylim = c(-100, 100)) +
+  theme_void()
+
+gg_logomark_black_on_white %>% 
+  ggsave(quarto_here("logomark_black_on_white.png"),
+         .,
+         width = 10,
+         height = 10)
+
+## ==== White on black
+
+gg_logomark_white_on_black <- ggplot() +
+  ## ==== graph
+  annotate("rect",
+           xmin = -100, xmax = 0, ymin = 0, ymax = 100,
+           fill = logomarkstroke_colour_geoms_colourful,
+           colour = cols_gpcds$graph_primary) +
+  annotate(
+    "segment",
+    x = -20, 
+    xend = -80,
+    y = 50, 
+    yend = 50,
+    linewidth = ident_linewidth + logomarkstroke_segment_adjustment,
+    colour = "white") +
+  annotate("point",
+           x = c(-20, -80),
+           y = c(50, 50),
+           size = ident_disk_size,
+           colour = "white",
+           fill = "white",
+           stroke = logomarkstroke_size,
+           pch = 21) +
+  annotate(
+    "segment",
+    x = -20, 
+    xend = -80,
+    y = 50, 
+    yend = 50,
+    linewidth = ident_linewidth + 2,
+    colour = "white") +
+  ## ==== plot
+  annotate("rect",
+           xmin = 0, xmax = 100, ymin = 0, ymax = 100,
+           fill = logomarkstroke_colour_geoms_colourful,
+           colour = cols_gpcds$plot_primary) +
+  annotate("point",
+           x = c(30, 70),
+           y = c(70, 30),
+           size = ident_disk_size,
+           colour = "white",
+           fill = "white",
+           stroke = logomarkstroke_size,
+           pch = 21) +
+  ## ==== chart
+  annotate("rect",
+           xmin = -100, xmax = 0, ymin = -100, ymax = 0,
+           fill = logomarkstroke_colour_geoms_colourful,
+           colour = cols_gpcds$chart_primary) +
+  geom_circle(aes(x0 = -50, y0 = -50, r = chart_minor_center_circle_radius),
+              linewidth = logomarkstroke_size - 1,
+              colour = "white",
+              fill = "white") +
+  annotate("point",
+           x = -chart_equ_solution,
+           y = -chart_equ_solution,
+           size = ident_disk_size,
+           colour = logomarkstroke_colour_geoms_colourful,
+           fill = "white",
+           stroke = logomarkstroke_size,
+           pch = 21) +
+  ## ==== story
+  annotate("rect",
+           xmin = 0, xmax = 100, ymin = 0, ymax = -100,
+           fill = logomarkstroke_colour_geoms_colourful,
+           colour = cols_gpcds$story_primary) +
+  annotate("point",
+           x = c(25, 75),
+           y = -50,
+           size = ident_disk_size,
+           colour = "white",
+           fill = "white",
+           stroke = logomarkstroke_size,
+           pch = 21) +
+  annotate("curve",
+           x = logomarkstory_x_coords$min + logomarkstory_arrow_x_adjustment,
+           y = -50 + logomarkstory_arrow_y_adjustment,
+           xend = logomarkstory_x_coords$max - logomarkstory_arrow_x_adjustment,
+           yend = -50 + logomarkstory_arrow_y_adjustment,
+           linewidth = ident_linewidth + logomarkstroke_curve_adjustment,
+           colour = "white",
+           arrow = arrow(length = unit(0.6, "cm"), type = "closed"),
+           curvature = -0.5,
+           ncp = 1000) +
+  annotate("curve",
+           x = logomarkstory_x_coords$min + logomarkstory_arrow_x_adjustment,
+           y = -50 + logomarkstory_arrow_y_adjustment,
+           xend = logomarkstory_x_coords$max - logomarkstory_arrow_x_adjustment,
+           yend = -50 + logomarkstory_arrow_y_adjustment,
+           linewidth = ident_linewidth - logomarkstroke_curve_adjustment,
+           colour = "white",
+           arrow = arrow(length = unit(0.6, "cm"), type = "closed"),
+           curvature = -0.5,
+           ncp = 1000) +
+  annotate("curve",
+           x = 100 - logomarkstory_x_coords$min - logomarkstory_arrow_x_adjustment,
+           y = -50 - logomarkstory_arrow_y_adjustment,
+           xend = 100 - logomarkstory_x_coords$max + logomarkstory_arrow_x_adjustment,
+           yend = -50 - logomarkstory_arrow_y_adjustment,
+           linewidth = ident_linewidth + logomarkstroke_curve_adjustment,
+           colour = "white",
+           arrow = arrow(length = unit(0.6, "cm"), type = "closed"),
+           curvature = -0.5,
+           ncp = 1000) +
+  annotate("curve",
+           x = 100 - logomarkstory_x_coords$min - logomarkstory_arrow_x_adjustment,
+           y = -50 - logomarkstory_arrow_y_adjustment,
+           xend = 100 - logomarkstory_x_coords$max + logomarkstory_arrow_x_adjustment,
+           yend = -50 - logomarkstory_arrow_y_adjustment,
+           linewidth = ident_linewidth - logomarkstroke_curve_adjustment,
+           colour = "white",
+           arrow = arrow(length = unit(0.6, "cm"), type = "closed"),
+           curvature = -0.5,
+           ncp = 1000) +
+  ## ==== seperators 
+  geom_segment(aes(y = -100, yend = 100, x = 0, xend = 0),
+               colour = "white",
+               linewidth = logomarkstroke_size - 2) +
+  geom_segment(aes(x = -100, xend = 100, y = 0, yend = 0),
+               colour = "white",
+               linewidth = logomarkstroke_size - 2) +
+  annotate("rect",
+           xmin = -100, xmax = 100, ymin = -100, ymax = 100,
+           fill = NA,
+           colour = "white",
+           linewidth = logomarkstroke_size) +
+  ## ==== scales
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  coord_fixed(xlim = c(-100, 100), ylim = c(-100, 100)) +
+  theme_void()
+
+gg_logomark_white_on_black %>% 
+  ggsave(quarto_here("logomark_white_on_black.png"),
+         .,
+         width = 10,
+         height = 10)
+
+
+# combination mark --------------------------------------------------------
+
+gg_combination_mark_colour <- gg_logomark_colour +
+  annotate("text",
+           label = "G|P|C|DS",
+           size = 45,
+           family = "Futura",
+           x = 0,
+           y = -130) +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  coord_fixed(xlim = c(-130, 130), ylim = c(-175, 115)) +
+  theme_void()
+
+gg_combination_mark_colour %>% 
+  ggsave(quarto_here("combination_mark_colour.png"),
+         .,
+         width = 10,
+         height = 10,
+         bg = "white")
+
+
